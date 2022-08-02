ebfa581da120f6e1addc44b4ea9a06951db9c784
Damien FURIET <damien@furiet.net>
2022-08-02 13:15:40 +0200

    exo3

--- app.dart
+++ app.dart
@@ -2,26 +2,59 @@ import 'dart:math';
 
 import 'NE_PAS_TOUCHER/user_input.dart';
 import 'bot.dart';
+import 'player.dart';
 
 void main() {
-  print("Bonjour!");
-  String nickname = readText("Quel est votre nom ?");
-
   final bot = Bot();
+  final player = Player();
+  player.pseudo = readText("Quel est votre nom ?");
+  var isMyTour = Random().nextBool();
   int i = 0;
+
   do {
-    readText("$nickname : Appuyer sur entrée pour lancer les des");
-    final de = lancerDes();
-    print("$nickname : Dé = $de");
-    bot.health = bot.health - de;
-    print("$nickname : Force = ${bot.health}");
+    readText(player.pseudo + " : Appuyer sur entrée pour un prochin tour");
+    if (isMyTour) {
+      attackPlayer(player, bot);
+      displayBot(bot);
+    } else {
+      attackBot(bot, player);
+      displayPlayer(player);
+    }
+    isMyTour = !isMyTour;
     i++;
-  } while (bot.health > 0);
-  print("Gagné en $i coup !");
+  } while (bot.health >= 0 && player.health >= 0);
+
+  if (bot.health < 0) {
+    print("Gagné en $i coup !");
+  }
+  if (player.health < 0) {
+    print("Perdu en $i coups");
+  }
 }
 
 int lancerDes() {
   final generator = Random();
   final de = generator.nextInt(12) + 1;
   return de;
+}
+
+void displayPlayer(Player player) {
+  print(player.pseudo +
+      " : force = ${player.strength}, Santé = ${player.health}%");
+}
+
+void displayBot(Bot bot) {
+  print("Bot : Force = ${bot.strength}, Santé = ${bot.health}%");
+}
+
+void attackPlayer(Player player, Bot bot) {
+  final dePlayer = lancerDes();
+  print(player.pseudo + " : Dé = $dePlayer");
+  bot.health -= dePlayer;
+}
+
+void attackBot(Bot bot, Player player) {
+  final deBot = lancerDes();
+  print("Bot : Dé = $deBot");
+  player.health -= deBot;
 }
---
+++ player.dart
@@ -0,0 +1,5 @@
+class Player {
+  String pseudo = "";
+  int strength = 1;
+  int health = 100;
+}
