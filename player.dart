ff4eb5c4acc003d0f7b9401bed8b1dcc470aeda2
Damien FURIET <damien@furiet.net>
2022-08-10 13:38:42 +0200

    exo5

--- player.dart
+++ player.dart
@@ -1,10 +1,12 @@
 import 'bot.dart';
 import 'app.dart';
+import 'weapon.dart';
 
 class Player {
   final String pseudo;
   int strength = 0;
   int health = 0;
+  Weapon _weapon = const Weapon("Batte de baseball", 1, 100);
 
   Player(this.pseudo);
 
@@ -15,14 +17,20 @@ class Player {
   }
 
   void attack(Bot bot) {
-    final dePlayer = lancerDes();
-    print(this.pseudo + " : Dé = $dePlayer");
-    bot.health -= dePlayer;
+    final randomPercent = Random().nextInt(100) + 1;
+    final dePlayer = lancerDes() + (strength + _weapon.power);
+    if (randomPercent <= _weapon.accuracy) {
+      print(this.pseudo + " : Dé = $dePlayer");
+      bot.health -= dePlayer;
+    } else {
+      print("Coup manquée .. : Dé = $dePlayer");
+    }
   }
 
   void victory(Bot bot, int i) {
+    const newWeapon = Weapon("Fusil à pompe", 2, 75);
     print(
-        "Bravo ${this.pseudo}, vous avez gagné en $i coups. J'augmente votre force !");
+        "Bravo ${this.pseudo}, vous avez gagné en $i coups. J'augmente votre force et vous avez un ${newWeapon.description} !");
     this.strength += 1;
     this.health = 100;
     bot.health = 100;
